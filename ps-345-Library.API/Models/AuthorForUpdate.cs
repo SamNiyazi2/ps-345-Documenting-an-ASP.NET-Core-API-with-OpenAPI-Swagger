cf22b907c227fd6e710a58201c7b019a966d6286
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-03-11 21:40:35 -0600

    Add data annotations.

--- ps-345-Library.API/Models/AuthorForUpdate.cs
+++ ps-345-Library.API/Models/AuthorForUpdate.cs
@@ -1,9 +1,23 @@
-﻿namespace Library.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.API.Models
 {
+
+    // 03/11/2022 09:33 pm - SSN - [20220311-2130] - [001] - M03-07 - Demo - Improving documentation with data annotations
+
     public class AuthorForUpdate
     {  
+
+        /// <summary>The firt name of the author</summary>
+        [Required]
+        [MaxLength(150)]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The last name of the author
+        /// </summary>
+        [Required]
+        [MaxLength(150)]
         public string LastName { get; set; }
     }
 }
