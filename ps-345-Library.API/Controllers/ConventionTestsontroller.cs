400e620d50016135006ea81d77dd380372af0152
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-03-12 21:46:23 -0600

    Add custom convention.  Not a best pratice.

--- ps-345-Library.API/Controllers/ConventionTestsontroller.cs
+++ ps-345-Library.API/Controllers/ConventionTestsontroller.cs
@@ -12,7 +12,9 @@ namespace Library.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     // [20220312-1544] - [001]
-    [ApiConventionType(typeof(DefaultApiConventions))]
+    // 03/12/2022 09:19 pm - SSN - [20220312-1849] - [003] - M04-07 - Creating custom conventions
+    //[ApiConventionType(typeof(DefaultApiConventions))]
+    [ApiConventionType(typeof(CustomConventions.CustomConvention))]
     public class ConventionTestsontroller : ControllerBase
     {
         // GET: api/<ConventionTestsontroller>
@@ -32,11 +34,18 @@ namespace Library.API.Controllers
             return "value";
         }
 
+
         // POST api/<ConventionTestsontroller>
         [HttpPost]
-        public void Post([FromBody] string value)
+        // 03/12/2022 06:49 pm - SSN - [20220312-1849] - [001] - M04-07 - Creating custom conventions
+        // Produces only 200 without the CustomConvention
+        // [20220312-1849] - [003]
+        //[ApiConventionMethod(typeof(CustomConventions.CustomConvention), nameof(CustomConventions.CustomConvention.Insert))]
+        //public void Post([FromBody] string value)
+        public void InsertTest([FromBody] string value)
         {
         }
+
 
         // PUT api/<ConventionTestsontroller>/5
         [HttpPut("{id}")]
---
+++ ps-345-Library.API/CustomConventions/CustomConvention.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 03/12/2022 08:59 pm - SSN - [20220312-1849] - [002] - M04-07 - Creating custom conventions
+
+namespace Library.API.CustomConventions
+{
+    public static class CustomConvention
+    {
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Insert(
+            [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+            [ApiConventionTypeMatch(ApiConventionTypeMatchBehavior.Any)]
+            object model)
+        {
+
+        }
+    }
+}
